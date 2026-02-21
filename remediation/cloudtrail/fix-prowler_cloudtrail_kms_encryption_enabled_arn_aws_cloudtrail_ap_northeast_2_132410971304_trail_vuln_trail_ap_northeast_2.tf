23ebd034fbca4817ec1539902410b250ec6aec07
jiwonseok97 <jjjws5670@gmail.com>
2026-02-21 11:24:43 +0900

    Fix cloudtrail remediation apply: remove duplicate KMS alias resource

--- remediation/cloudtrail/fix-prowler_cloudtrail_kms_encryption_enabled_arn_aws_cloudtrail_ap_northeast_2_132410971304_trail_vuln_trail_ap_northeast_2.tf
+++ remediation/cloudtrail/fix-prowler_cloudtrail_kms_encryption_enabled_arn_aws_cloudtrail_ap_northeast_2_132410971304_trail_vuln_trail_ap_northeast_2.tf
@@ -58,11 +58,6 @@ resource "aws_kms_key" "fix_cloudtrail_kms_key_19040a1c46" {
   enable_key_rotation = true
 }
 
-resource "aws_kms_alias" "fix_cloudtrail_kms_alias_19040a1c46" {
-  name          = "alias/cloudtrail-remediation-vuln_trail"
-  target_key_id = aws_kms_key.fix_cloudtrail_kms_key_19040a1c46.key_id
-}
-
 resource "aws_cloudtrail" "fix_cloudtrail_19040a1c46" {
   name                          = "vuln-trail"
   s3_bucket_name                = "vuln-cloudtrail-ap-northeast-2-f9fd7730"
